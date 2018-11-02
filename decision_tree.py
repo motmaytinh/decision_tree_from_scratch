cb5e358a58b6c658208f22e881d2a09293c00ccf
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-11-02 16:56:39 +0700

    Add remainder

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-eps = np.finfo(float).eps
+# eps = np.finfo(float).eps
 
 class DecisionTree(object):
     def fit(self, data, target):
@@ -11,11 +11,28 @@ class DecisionTree(object):
         self.labels = target.unique()
 
 
+    def _remainder(self, attribute):
+        atr = list(attribute)[0]
+        grouped = attribute.groupby(atr)
+        groups_name = list(attribute[atr].unique())
+        count = dict(attribute[atr].value_counts())
+        total = attribute.count()[0]
+        remainder = 0
+        for group in groups_name:
+            entropy = self._entropy(list(grouped.get_group(group).iloc[:,1].value_counts()))
+            remainder += count[group]/total*entropy
+        return remainder
+
     def _entropy(self, values):
         total = np.sum(values)
         pb = [i/total for i in values]
-        return np.sum([-p*np.log2(p) for p in pb])
+        return np.sum([-p*np.log(p) for p in pb])
 
+def main():
+    a = DecisionTree()
+    data = pd.read_csv("data.csv")
+    tmp = pd.concat([data['outlook'],data['play']],axis=1)
+    print(a._remainder(tmp))
 
 if __name__ == "__main__":
     main()
