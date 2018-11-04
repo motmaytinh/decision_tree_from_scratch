1de953e1742b3581a5459a43405405e234861c10
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-11-04 18:02:27 +0700

    Add random forest

--- random_forest.py
+++ random_forest.py
@@ -7,11 +7,32 @@ class RandomForest(object):
         self.forest = []
 
     def fit(self, data, target, num_tree):
+        high = data.shape[0]
         for i in range(num_tree):
-            self.forest.append(DecisionTree().fit(data, target))
+            idx = np.random.randint(0,high,high//2)
+            self.forest.append(DecisionTree().fit(data.iloc[idx], target.iloc[idx]))
 
     def predict(self, data):
         votes = []
+        
         for tree in self.forest:
             votes.append(tree.predict)
 
+        return _most_common(lst)
+
+        
+    def _most_common(lst):
+        return max(set(lst), key=lst.count)
+
+def main():
+    clf = DecisionTree()
+    load = pd.read_csv("data.csv")
+    data = load.iloc[:,:-1]
+    target = load.iloc[:,-1]
+    clf.fit(data,target)
+    for i in range(12):
+        print(clf.predict(data.iloc[i,:]))
+
+
+if __name__ == "__main__":
+    main()
