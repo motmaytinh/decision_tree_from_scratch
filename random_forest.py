42f8cf4ad52358c65e7fc1769d2e1793d3127f1a
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-11-06 18:21:27 +0700

    add random attribute

--- random_forest.py
+++ random_forest.py
@@ -3,14 +3,26 @@ import pandas as pd
 from decision_tree import DecisionTree
 
 class RandomForest(object):
-    def __init__(self):
+    def __init__(self, num_tree, random_attribute = False):
         self.forest = []
+        self.num_tree = num_tree
+        self.random_attribute = random_attribute
 
-    def fit(self, data, target, num_tree):
+    def fit(self, data, target):
         high = data.shape[0]
-        for i in range(num_tree):
-            idx = np.random.randint(0,high,high//2)
+        n_col = data.count(1)[0]
+        for i in range(self.num_tree):
+            idx = np.random.choice(high, size=high//2, replace=False, p=None)
+            idx.sort()
             tree = DecisionTree()
+
+            if self.random_attribute:
+                col_idx = np.random.choice(n_col, size=2*n_col//3, replace=False, p=None)
+                col_idx.sort()
+                tree.fit(data.iloc[idx,col_idx], target.iloc[idx])
+            else:
+                tree.fit(data.iloc[idx], target.iloc[idx])
+
             tree.fit(data.iloc[idx], target.iloc[idx])
             self.forest.append(tree)
 
@@ -27,11 +39,11 @@ class RandomForest(object):
         return max(set(lst), key=lst.count)
 
 def main():
-    clf = RandomForest()
+    clf = RandomForest(num_tree = 5, random_attribute = True)
     load = pd.read_csv("mushrooms.csv")
     data = load.iloc[:,1:]
     target = load.iloc[:,0]
-    clf.fit(data,target,5)
+    clf.fit(data,target)
     for i in range(12):
         print(clf.predict(data.iloc[i,:]))
 
